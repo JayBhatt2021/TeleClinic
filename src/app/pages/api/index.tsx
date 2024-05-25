5d2dc260390e6e1c8a5599cbda8bb28f9262027a
Jay Bhatt <jay.bhatt98@gmail.com>
2024-05-25 10:50:11 -0400

    Create basic index.tsx

---
+++ src/app/pages/api/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="Created by Next app" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main>
+        <h1>Hello world</h1>
+      </main>
+    </>
+  );
+};
+
+export default Home;
