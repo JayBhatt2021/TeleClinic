158dbbe7f8ae3771a598ed7b237e1b16d99f2b5c
Jay Bhatt <jay.bhatt98@gmail.com>
2024-09-04 21:51:39 -0400

    Convert  tailwind.config.ts to default form

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,12 +3,7 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {
-      colors: {
-        primary: "#7856ff",
-        "dark-fill-2": "hsla(0,0%,100%,.14)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
