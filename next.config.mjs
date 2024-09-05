3c96036ac927ce2ec79a043ca7ab31d2818e7a34
Jay Bhatt <jay.bhatt98@gmail.com>
2024-09-04 22:56:32 -0400

    Add some configs to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
-/** @type {import('next').NextConfig} */
+// Check https://nextjs.org/docs/pages/api-reference/next-config-js to see more config options.
+
+/** @type {import("next").NextConfig} */
 const nextConfig = {
+  basePath: "",
+  crossOrigin: "anonymous",
   reactStrictMode: true,
 };
 
