bfb54481941de7adadcc2591c8d6f8f08e00969c
Jay Bhatt <jay.bhatt98@gmail.com>
2024-09-27 23:14:04 -0400

    Add comment to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from "tailwindcss";
 
+// See https://tailwindcss.com/docs/configuration#using-esm-or-type-script for more guildlines.
 const tailwindConfig: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
