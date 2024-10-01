53150482ef17252c90c1ea59f71cdd8b93b3c435
Jay Bhatt <jay.bhatt98@gmail.com>
2024-09-30 21:30:52 -0400

    Remove mdx from content in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 
 // See https://tailwindcss.com/docs/configuration#using-esm-or-type-script for more guildlines.
 const tailwindConfig: Config = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
