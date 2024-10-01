3fb9c9cfe032c77bc78c9ae347f32cbcd3cc2382
Jay Bhatt <jay.bhatt98@gmail.com>
2024-09-30 21:00:25 -0400

    Refactor lint-staged.config.js

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,13 +1,14 @@
 // Check https://github.com/lint-staged/lint-staged?tab=readme-ov-file#configuration to see more config options.
 const lintStagedConfig = {
-  // Runs type-checking on TypeScript files
+  // Runs type-checking on TS and TSX files to catch errors early
   "**/*.ts?(x)": "npm run check-types",
 
-  // Checks the formatting of and lints staged files to ensure consistency
-  "**/*.{js,ts,jsx,tsx,mdx,json}": [
-    "npm run check-format",
-    "npm run check-lint",
-  ],
+  // Checks the formatting of the specified files to ensure consistent code style
+  "**/*.{js,mjs,jsx,ts,tsx,json,css}": "npm run check-format",
+
+  // Lints the specified files to enforce code quality and consistency
+  "**/*.{js,mjs,jsx,ts,tsx}": "npm run check-lint",
 };
 
 export default lintStagedConfig;
+1;
